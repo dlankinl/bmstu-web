85ca445030abcfeefb8f2e976567f8d917acdde2
dmitry <dmit.lankin@mail.ru>
2024-12-20 15:05:36 +0300

    added financial report res

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,6 +8,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import CompanyInfoComponent from './components/CompanyInfo/CompanyInfo';
 import './App.css'
 import CompaniesList from './components/Companies/CompaniesList';
+import FinancialReportComponent from './components/FinancialReport/FinancialReport';
 
 function App() {
   // const [formType, setFormType] = useState<'company' | 'activity field'>('company');
@@ -26,6 +27,7 @@ function App() {
           <Route path="/activity-fields" element={<ActivityFieldsList isAdmin={true} />} />
           <Route path="/companies" element={<CompaniesList />} />
           {/* <Route path="/companies/create" element={<Form title="Создать компанию" fields={companyFields} onSubmit={handleSubmit} />} /> */}
+          <Route path="/financials/:id" element={<FinancialReportComponent />} />
         </Routes>
       </div>
     </Router>
